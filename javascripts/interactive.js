da79bad1bb4b3e74d445fd05930390c592bdf8f2
Cora Ceipek <cora.ceipek@gmail.com>
2014-07-16 00:21:16 -0400

    Automated commit at 2014-07-16 04:21:16 UTC by middleman-deploy 0.2.4

---
+++ javascripts/interactive.js
@@ -0,0 +1,28 @@
+$(function () {
+	var validate = function (arr) {
+		for (var i = 0; i < arr.length; i++) {
+			if (arr[i] !== i) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	var group = $('.sortable').sortable(
+		{
+			group: 'sortable',
+			nested: false,
+			vertical: false,
+			onDrop: function (item, container, _super) {
+				if(validate(container.el.sortable("serialize").get())) {
+					container.el.next(".js-activity-validate").show();
+				} else {
+					container.el.next(".js-activity-validate").hide();
+				}
+				_super(item, container);
+			},
+			serialize: function (parent, children, isContainer) {
+				return isContainer ? children : parseInt(parent.attr('data-value'))
+			}
+		});
+});
